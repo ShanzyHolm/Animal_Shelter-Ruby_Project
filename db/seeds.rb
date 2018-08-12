e635c4ce0705c3df1b6c75ffcffbc79f89f0ac60
Shannon Holmes <snholmes13@gmail.com>
2018-08-12 21:42:10 +0100

    add table info to shelter.sql

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,15 @@ require_relative('../models/animal.rb')
 require_relative('../models/owner.rb')
 require('pry')
 
+Adoption.delete_all()
+Owner.delete_all()
+Animal.delete_all()
 
+animal1 = (
+  {
 
+  }
+)
 
 binding.pry
 nil
--- db/shelter.sql
+++ db/shelter.sql
@@ -1,6 +1,31 @@
-DROP TABLE ;
+DROP TABLE adoptions;
+DROP TABLE animals;
+DROP TABLE owners;
 
-CREATE TABLE 
+CREATE TABLE animals
 (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) not null,
+  admission DATE,
+  adoptable BOOLEAN,
+  species VARCHAR(255),
+  -- age INT,
+  -- breed VARCHAR(255),
+  -- description TEXT,
+);
 
+CREATE TABLE owners
+(
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255) not null,
+  last_name VARCHAR(255) not null,
+  interest VARCHAR(255),
+  contact_info VARCHAR(255)
+);
+
+CREATE TABLE adoptions
+(
+  id SERIAL8 PRIMARY KEY,
+  animal_id INT references animals(id),
+  owner_id INT references owners(id)
 );
