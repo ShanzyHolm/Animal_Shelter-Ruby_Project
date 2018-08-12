06282e95860b32d75aa49770d0e2066809508198
Shannon Holmes <snholmes13@gmail.com>
2018-08-12 21:48:09 +0100

    add images file and initialize info for animal.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,21 @@ animal1 = (
 
 binding.pry
 nil
+
+
+
+CREATE TABLE owners
+(
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255) not null,
+  last_name VARCHAR(255) not null,
+  interest VARCHAR(255),
+  contact_info VARCHAR(255)
+);
+
+CREATE TABLE adoptions
+(
+  id SERIAL8 PRIMARY KEY,
+  animal_id INT references animals(id),
+  owner_id INT references owners(id)
+);
--- models/animal.rb
+++ models/animal.rb
@@ -6,8 +6,22 @@ class Animal
   # attr_writer
   # attr_accessor
 
-  def initialize()
-
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @admission = options['admission']
+    @adoptable = options['adoptable']
+    @species = options['species']
   end
 
 end
+
+
+# id SERIAL8 PRIMARY KEY,
+# name VARCHAR(255) not null,
+# admission DATE,
+# adoptable BOOLEAN,
+# species VARCHAR(255),
+# age INT,
+# breed VARCHAR(255),
+# description TEXT,
