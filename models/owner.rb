c71ea7d749fda54cc7400f537245f84a578c804e
Shannon Holmes <snholmes13@gmail.com>
2018-08-13 13:34:17 +0100

    add methods in owner class

--- models/owner.rb
+++ models/owner.rb
@@ -20,4 +20,41 @@ class Owner
     results = SqlRunner.run(sql, values)
     @id = results.first()['id'].to_i
   end
+
+  def update()
+    sql = "UPDATE owners SET (first_name, last_name) = ($1, $2) WHERE id = $6"
+    values = [@first_name, @last_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM owners WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def owner_name()
+    return "#{@first_name} #{@last_name}"
+  end
+
+  def self.all()
+    sql = "SELECT * FROM owners"
+    owners = SqlRunner.run(sql)
+    results = owners.map { |owner| Owner.new(owner) }
+    return result
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM owners WHERE id = $1"
+    values = [id]
+    owner = SqlRunner.run(sql, values)
+    result = Owner.new(owner.first())
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM owners"
+    SqlRunner.run(sql)
+  end
+
 end
