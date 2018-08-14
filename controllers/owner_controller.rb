956d15585c6c396da66104f88c4db2348cda3703
Shannon Holmes <snholmes13@gmail.com>
2018-08-14 03:07:02 +0100

    format html owners show.erb

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -4,3 +4,13 @@ require_relative('../models/owner.rb')
 require_relative('../models/animal.rb')
 require_relative('../models/adoption.rb')
 also_reload('../models/*')
+
+get '/owners/show' do
+  @owners = Owner.all()
+  erb (:"owners/show")
+end
+
+get '/owners/:id' do
+  @owner = Owner.find(params[:id])
+  erb (:"owners/index")
+end
